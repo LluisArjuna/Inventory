632ef3f1988a9fb2e066270bb805ffebcbde495c
LluisArjuna <lluisarjunav@gmail.com>
2026-05-13 13:06:39 +0200

    feat(config): add Firebase environment configuration and provisional API environment variable

---
+++ src/environments/environment.ts
@@ -0,0 +1,22 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:8080/api',
+  firebase: {
+    apiKey: 'AIzaSyBfHytGb8ogGfdZlW4DCcutem_UFFdGKI4',
+    authDomain: 'inventory-45207.firebaseapp.com',
+    projectId: 'inventory-45207',
+    storageBucket: 'inventory-45207.firebasestorage.app',
+    messagingSenderId: '331662419732',
+    appId: '1:331662419732:web:5c9932da638b19dbd3c401',
+    measurementId: 'G-X345ST877F'
+  }/*
+  firebase: {
+    apiKey: 'FIREBASE_API_KEY',
+    authDomain: 'FIREBASE_AUTH_DOMAIN',
+    projectId: 'FIREBASE_PROJECT_ID',
+    storageBucket: 'FIREBASE_STORAGE_BUCKET',
+    messagingSenderId: 'FIREBASE_MESSAGING_SENDER_ID',
+    appId: 'FIREBASE_APP_ID',
+    measurementId: 'FIREBASE_MEASUREMENT_ID'
+  }*/
+};
