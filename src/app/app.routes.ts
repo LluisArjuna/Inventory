36c9941131e6ea31921e61843b309314b005e66e
LluisArjuna <lluisarjunav@gmail.com>
2026-05-14 13:32:36 +0200

    feat(router): configure authentication routes with access guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { authGuard, guestGuard } from './core/guards/auth.guard';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Login, 
+    canActivate: [authGuard],
+  },
+  {
+    path: 'login',
+    component: Login,
+    canActivate: [guestGuard]
+  },
+  {
+    path: 'register',
+    component: Register,
+    canActivate: [guestGuard]
+  }
+];
