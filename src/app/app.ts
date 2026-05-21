9ceaa3c39edcf1c93101f8e851e85ccf7eef099a
LluisArjuna <lluisarjunav@gmail.com>
2026-05-21 12:43:10 +0200

    feat(ui): add reactive state for create inventory dialog visibility

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from '@shared/components/navbar/navbar';
+import { CreateInventory } from '@features/inventories/create-inventory/create-inventory';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, CreateInventory],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('inventory');
+  protected readonly showCreateDialog = signal(false);
 }
